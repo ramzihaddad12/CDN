a518f248a34dca3cb151f9d35961a45e8d48d4fd
ramzihaddad12 <ramzi.haddad12@gmail.com>
2022-11-19 20:46:15 -0500

    dnsserver done

--- constants.py
+++ constants.py
@@ -6,3 +6,10 @@ dns_server_ip_address = "97.107.140.73"
 # List of replicas (we only have 1 atm)
 replicas = ['proj4-repl1.5700.network']
 replicas_by_IP = ['139.144.30.25']
+
+beacon = "https://cdn-beacon.5700.network/" # /grading/beacon
+
+origin_server = "cs5700cdnorigin.ccs.neu.edu"
+origin_port = 8080
+
+build_server = "cs5700cdnproject.ccs.neu.edu"
