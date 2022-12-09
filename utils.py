d6bce073da8f19d3d8a0542b2e0391f86778160c
John Ciolfi <johnc2744@gmail.com>
2022-12-09 10:26:30 -0500

    encode article in build_url

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 from constants import *
+import urllib.parse
 
 def build_url(uri, host=ORIGIN_SERVER, port=SERVER_PORT):
-    return f'http://{host}:{port}/{uri}'
+    return f'http://{host}:{port}/{urllib.parse.quote(uri)}'
 
 def bad_status(status):
     return status >= 300 or status < 200
