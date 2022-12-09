c91fdb1eedbb1efd52d18844c2a9e83ae8a236b0
John Ciolfi <johnc2744@gmail.com>
2022-12-09 10:58:58 -0500

    add url encoding to cacher

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 from constants import *
-import urllib.parse
 
 def build_url(uri, host=ORIGIN_SERVER, port=SERVER_PORT):
-    return f'http://{host}:{port}/{urllib.parse.quote(uri)}'
+    return f'http://{host}:{port}/{uri}'
 
 def bad_status(status):
     return status >= 300 or status < 200
